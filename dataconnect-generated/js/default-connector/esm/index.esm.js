d61390359fb8e5793edeb5e5609d2b14a41164e7
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2025-04-28 23:26:51 -0400

    added firebase to deploy

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'ligadota2peru',
+  location: 'us-central1'
+};
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 🔥 Firebase imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // 🔥 Inicialización de Firebase
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()) // para usar Firestore
   ],
   providers: [],
   bootstrap: [AppComponent]
